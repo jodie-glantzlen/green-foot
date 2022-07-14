45f46b23248c3a73f428b65ea89a69467282cca9
kais-lutfi <103362729+kais-lutfi@users.noreply.github.com>
2022-07-15 09:46:48 +1200

    Give <Nav /> a basic style

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Nav () {
+function Nav() {
   return (
     <nav>
-      <h1>Green Foot</h1>
-      <img src="/Greenfoot.png" alt="Green Foot logo" />
-      <div>
+      <div className="branding">
+        <img src="/Greenfoot.png" alt="Green Foot logo" />
+      </div>
+      <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/mychallenges">My Challenges</Link>
       </div>
