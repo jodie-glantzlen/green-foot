42a113a1a8f83affe56e0192ed30783a4f75c1dd
kais-lutfi <103362729+kais-lutfi@users.noreply.github.com>
2022-07-14 16:41:26 +1200

    challenge component complete

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const path = require('path')
 
-const challengeRoutes = require('./routes/mychallenges')
+const myChallengeRoutes = require('./routes/mychallenges')
+const challengeRoutes = require('./routes/challenges')
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
+server.use(express.static(path.join(__dirname, 'public')));server.use('/api/v1/challenges', challengeRoutes)
+server.use('/api/v1/challenges', myChallengeRoutes)
+
 
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
 })
 
-server.use('/api/v1/challenges', challengeRoutes)
+
 
 module.exports = server
