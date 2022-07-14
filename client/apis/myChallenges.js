73cea51c95d169cd6726e611331635f5315b3a3b
Tayla Dunlop <103092915+tayla-dunlop@users.noreply.github.com>
2022-07-15 10:06:33 +1200

    add challenge from home to my challenges page

--- client/apis/myChallenges.js
+++ client/apis/myChallenges.js
@@ -4,8 +4,11 @@ const rootUrl = '/api/v1/challenges'
 
 export function  fetchAllMyChallenges() {
   return request.get(rootUrl + '/mychallenges')
-    .then(res => {
-     return res.body
+    .then(res =>  res.body)
+}
 
-    })
+export function postMyChallenge(selectedChallengeId) {
+  return request.post(rootUrl + '/mychallenges')
+  .send(selectedChallengeId)
+  .then(res => res.body)
 }
