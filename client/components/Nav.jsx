d9b6757ce00699691ff597494c1ffb4d886dbeec
Jodie Glantzlen <68207139+jodie-glantzlen@users.noreply.github.com>
2022-07-16 15:14:56 +1200

    Remove logo from navbar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav className="navbar">
-      <img src="/Greenfoot.png" alt="Green Foot logo" />
       <Link to="/">Home</Link>
       <Link to="/mychallenges">My Challenges</Link>
     </nav>
