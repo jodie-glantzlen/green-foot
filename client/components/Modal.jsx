d883aad064cf8384167413cdc21dd8b57d367255
Jodie Glantzlen <68207139+jodie-glantzlen@users.noreply.github.com>
2022-07-18 15:25:29 +1200

    Add cool overlay effect

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -7,23 +7,26 @@ function Modal({ data , viewModal, handleClick }) {
 
 
   return (
-    <div className="banaa">
-      <div className="banaa-content">
-        <p className="title is-4">{data.title}</p>
-        <p>{data.description}</p>
-        <button className="button is-small is-rounded">
-          {data.points} points
-        </button>
-        <button className="button is-small is-rounded"
-          onClick={evt => handleClick(evt)}>
-          Accept
-        </button>
-        <button className="button is-small is-rounded"
-          onClick={evt => viewModal(evt)}>
-          Close
-        </button>
+    <div className="overlay">
+      <div className="banaa">
+        <div className="banaa-content">
+          <p className="title is-4">{data.title}</p>
+          <p>{data.description}</p>
+          <button className="button is-small is-rounded">
+            {data.points} points
+          </button>
+          <button className="button is-small is-rounded"
+            onClick={evt => handleClick(evt)}>
+            Accept
+          </button>
+          <button className="button is-small is-rounded"
+            onClick={evt => viewModal(evt)}>
+            Close
+          </button>
+        </div>
       </div>
     </div>
+
   )
 }
 
--- client/components/RankPopup.jsx
+++ client/components/RankPopup.jsx
@@ -3,13 +3,14 @@ import React from "react";
 function RankPopup ({viewRankPopup, rank}) {
 
   return (
-    <div className="banaa">
-      <div className="banaa-content">
-        <p className="title is-4">{rank}</p>
-  
-        <button className="button is-small is-rounded" onClick={evt => viewRankPopup(evt)}>
-          Close
-        </button>
+    <div className="overlay">
+      <div className="banaa">
+        <div className="banaa-content">
+          <p className="title is-4">{rank}</p>
+          <button className="button is-small is-rounded" onClick={evt => viewRankPopup(evt)}>
+            Close
+          </button>
+        </div>
       </div>
     </div>
   )
