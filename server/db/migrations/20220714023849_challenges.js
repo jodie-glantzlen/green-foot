5e5677c043d42efc512c8ceac99f2c677300d3ee
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-07-27 21:28:30 +1200

    Fix eslint stuff

--- server/db/migrations/20220714023849_challenges.js
+++ server/db/migrations/20220714023849_challenges.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('challenges', table => {
     table.increments('id')
     table.string('title')
@@ -8,6 +8,6 @@ exports.up = function(knex) {
   })
 }
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('challenges')
 }
